1af3ae7678b4f641a0cd7c447d72ec5ca55984c3
Adriana Mundo <adrianamundog@hotmail.com>
2019-10-08 10:54:28 -0600

    Readme actualizado

--- bio.py
+++ bio.py
@@ -12,8 +12,10 @@ environment=os.getenv("ENVIRONMENT","development")
 def about():
     images ={"profile" : url_for('static', filename = 'images/profille.jpeg'),}
     links = info['links']
-    return render_template("index.html", links = links, info=info, images = images)
-
+    date = datetime.now()
+    date = str(date)
+    return render_template("index.html", links = links, info=info, images = images, date = date)
+      
 if __name__ == "__main__":
     debug=False
     if environment == "development" or environment == "local":
